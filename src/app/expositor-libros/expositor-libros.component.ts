ff982676c51bde19b40a8ebe170b240c1cfe6f0a
Maunrren <adrianconde90@gmail.com>
2023-05-17 16:40:16 +0200

    first commit

---
+++ src/app/expositor-libros/expositor-libros.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expositor-libros',
+  templateUrl: './expositor-libros.component.html',
+  styleUrls: ['./expositor-libros.component.scss']
+})
+export class ExpositorLibrosComponent {
+
+}
