f4e93ba629032c9d4786ddbb2bf6b8e0a40aa2e9
Maunrren <adrianconde90@gmail.com>
2023-05-17 16:43:12 +0200

    comentario de prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExpositorLibrosComponent } from './expositor-libros/expositor-libros.component';
-
+//cositas
 @NgModule({
   declarations: [
     AppComponent,
